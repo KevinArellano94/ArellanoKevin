9e0452c2e5efa1bb144e4c68a999a753e6bd4d0d
Kevin Arellano <Kevin.Arellano94@gmail.com>
2020-11-18 17:56:06 -0500

    Create program.js

---
+++ javascript/program.js
@@ -0,0 +1,18 @@
+document.writeln('<meta charset="UTF-8" />');
+document.writeln('<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">');
+document.writeln('<meta http-equiv="X-UA-Compatible" content="ie=edge" />');
+document.writeln('<link rel="stylesheet" href="styles.css">');
+document.writeln('<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">');
+
+//document.writeln('<button onclick="homepage()">Home</button>');
+//document.writeln('<button onclick="aboutpage()">About</button>');
+
+//document.writeln('<button onclick="homepage()">Home</button>');
+
+//function homepage() {
+//    location.replace("index.html");
+//  }
+
+//function aboutpage() {
+//    location.replace("about.html");
+//  }

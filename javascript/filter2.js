fbafdc6df614d5528d8bb3edcc0b5209edc7b1a3
Kevin Arellano <Kevin.Arellano94@gmail.com>
2020-11-21 16:26:18 -0500

    Uploaded V-2 ( 4 files )
    Filter2.js - index.html search function
    Filter.js - games.html search function
    game_2DBats.js - Flappy Birds process | Documented by ( https://www.ansoriweb.com/2020/03/javascript-game.html )
    navigation.js - navigation bar style and function for all pages

---
+++ javascript/filter2.js
@@ -0,0 +1,13 @@
+function myFilter()
+{
+    
+$(document).ready(function(){
+  $("#listSearch").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#myList li").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
+}
+
---
+++ javascript/navigation.js
@@ -0,0 +1,37 @@
+document.writeln('<nav class="navbar navbar-expand-lg navbar-light bg-light">');
+
+document.writeln('<button');
+document.writeln('class="navbar-toggler"');
+document.writeln('type="button"');
+document.writeln('data-toggle="collapse"');
+document.writeln('data-target="#navbarSupportedContent"');
+document.writeln('aria-controls="navbarSupportedContent"');
+document.writeln('aria-expanded="false"');
+document.writeln('aria-label="Toggle navigation">');
+document.writeln('<span class="navbar-toggler-icon"></span>');
+document.writeln('</button>');
+
+document.writeln('<div class="collapse navbar-collapse" id="navbarSupportedContent">');
+
+document.writeln('<ul class="navbar-nav mr-auto">');
+
+//HOME PAGE//
+document.writeln('<li class="nav-item active">');
+document.writeln('<a class="nav-link" href="index.html">HOME</a>');
+document.writeln('</li>');
+
+//ABOUT PAGE//
+document.writeln('<li class="nav-item active">');
+document.writeln('<a class="nav-link" href="about.html">ABOUT</a>');
+document.writeln('</li>');
+
+//GAMES PAGE//
+document.writeln('<li class="nav-item active">');
+document.writeln('<a class="nav-link" href="games.html">GAMES</a>');
+document.writeln('</li>');
+
+document.writeln('</ul>');
+
+document.writeln('</div>');
+
+document.writeln('</nav>');
